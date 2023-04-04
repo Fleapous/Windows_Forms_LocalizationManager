da057854bf7126ae65c5ad353dd165e7829737a5
egecan giray <girayegecan02@gmail.com>
2023-04-05 00:02:42 +0200

    adding files works properly

--- Windows_Forams_LocManager/Form1.cs
+++ Windows_Forams_LocManager/Form1.cs
@@ -15,12 +15,18 @@ namespace Windows_Forams_LocManager
     public partial class Form1 : Form
     {
         //global vars :(
+
+        //lists for entries
         List<DialogEntry> entries = new List<DialogEntry>();
+        List<DialogEntry> allEntries = new List<DialogEntry>();
+
+        //for adding new dirs and files
         private TreeNode subNode, parentNode;
         string path;
         private DialogEntry newEntry = new DialogEntry();
 
-
+        //for editing files 
+        private string globalTag;
 
 
         public Form1()
@@ -30,6 +36,7 @@ namespace Windows_Forams_LocManager
             treeView1.LabelEdit = true;
         }
 
+        //de serialization and tree making
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
             entries.Clear();
@@ -47,25 +54,26 @@ namespace Windows_Forams_LocManager
 
                 //treeView1.Nodes.Add(filePath);
                 entries = DeSerializer(filePath);
+                allEntries = entries;
                 TreeMaker(entries);
             }
         }
+        //displays the details of sllected tag ie node
         private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
         {
-            //if (rightClick)
-            //{
-            //    // Show the context menu
-            //    //MessageBox.Show("flag is true");
-            //    contextMenuStrip1.Show(treeView1, treeView1.PointToClient(Control.MousePosition));
-            //    //get the node
-            //    currentNode = e.Node;
-            //    rightClick = false;
-            //}
-
+            globalTag = null;
             NametranslationList.Items.Clear();
+            NameDescriptTextBox.Text = string.Empty;
+            NamePathTextBox.Text = string.Empty;
+
+
             var selectedNode = e.Node;
             var tag = selectedNode.Tag;
-            DialogEntry file = entries.FirstOrDefault(o => o.LocKey == (string)tag);
+
+            //setting the global tag as the tag thats been selected
+            globalTag = (string)selectedNode.Tag;
+
+            DialogEntry file = allEntries.FirstOrDefault(o => o.LocKey == (string)tag);
 
             if (file == null)
             {
@@ -84,6 +92,8 @@ namespace Windows_Forams_LocManager
                 NametranslationList.Items.Add(newItem);
             }
         }
+
+        //search bar 
         private void NameSearchButton_Click(object sender, EventArgs e)
         {
             NameSearchList.Items.Clear();
@@ -91,7 +101,7 @@ namespace Windows_Forams_LocManager
             string searchBar = NameSearchBar.Text;
 
             //look for the given name in the entries list and add all that matches the searchBar string to the list
-            List<DialogEntry> resoult = entries.FindAll(s => s.EntryName.Contains(searchBar));
+            List<DialogEntry> resoult = allEntries.FindAll(s => s.EntryName.Contains(searchBar));
 
             foreach(DialogEntry res in resoult)
             {
@@ -221,6 +231,7 @@ namespace Windows_Forams_LocManager
             if (subNode != null)
             {
                 //MessageBox.Show(path);
+
                 //set the pre text 
                 NamePathTextBox.Text = path;
                 // setthe carrot
@@ -240,18 +251,78 @@ namespace Windows_Forams_LocManager
         {
             if(e.KeyCode == Keys.Enter)
             {
+                // get the textbox text and find the name of the file from it ie last dir
+                string[] splited = NamePathTextBox.Text.Split("-");
+                string name = splited[splited.Length - 1]; 
+
                 //sets the path of the new entry
                 DialogEntry tmp = new DialogEntry();
+                Translations tmpT = new Translations();
                 tmp.HierarchyPath = NamePathTextBox.Text;
+                tmp.LocKey = Guid.NewGuid().ToString("N");
+                tmp.EntryName = name;
+                tmpT.Debug = "tmp";
+                tmp.Translations = tmpT;
                 newEntry = tmp;
+
+                //set the global tag 
+                globalTag = tmp.LocKey;
+
+                //add the new file to the tree view
+                var newNode = new TreeNode();
+                newNode.Tag = tmp.LocKey;
+                newNode.Text = tmp.EntryName;
+                newNode.ImageIndex = 1;
+                subNode.Nodes.Add(newNode);
+
+                // add new entry to all entries list
+                allEntries.Add(newEntry);
+
+                NamePathTextBox.ReadOnly = true;
                 //shifts focus to description box
                 NameDescriptTextBox.Focus();
             }
         }
 
+        private void NameDescriptTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+
+            }
+        }
+
+
+        //updates the sellected files details with each key stroke
+        //global tag connects file to the textbox soo be carefull and dont forget to set it to null before making any changes to the textbox
+        private void NameDescriptTextBox_TextChanged(object sender, EventArgs e)
+        {
+            //set the tag in a local var
+            string tag = globalTag;
+
+            //search for the tag
+            DialogEntry file = allEntries.FirstOrDefault(o => o.LocKey == tag);
+
+            //check if the file exists
+            if (file != null && tag != null)
+            {
+                //overwrite the debug
+                file.Translations.Debug = NameDescriptTextBox.Text;
+
+                //updating the list view this aproach creates 2 rows unitl the textbox updates
+                NametranslationList.Items.Clear();
+                string translationText = file.Translations.Debug;
+                string translationLanguage = "Debug";
+                ListViewItem newItem = new ListViewItem(new[] { translationLanguage, translationText });
+                NametranslationList.Items.Add(newItem);
+            }
+            tag = null;
+        }
+
+        //selects the node thats clicked on and saves it to a global var named subNode 
         private void treeView1_MouseClick(object sender, MouseEventArgs e)
         {
-            if (treeView1.GetNodeAt(e.Location) != null)
+            if (treeView1.GetNodeAt(e.Location) != null && treeView1.GetNodeAt(e.Location).Tag == null)
             {                
 
                 // pre alocating node and sub node and a tmp node
@@ -277,6 +348,12 @@ namespace Windows_Forams_LocManager
                     //show menu strip
                     contextMenuStrip1.Show(e.Location);
                 } 
+            }
+            else
+            {
+                subNode = null;
+                parentNode = null;
+                path = null;
             }
         }
     }
